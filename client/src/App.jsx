9f769e9d7ef490b8b86bb208c23b519b28ef2341
DinoSage <dinosage5632@gmail.com>
2025-03-31 18:57:43 -0400

    [+] Fix Broken Server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="w-full p-6">
